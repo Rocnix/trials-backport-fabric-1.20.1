b36481565e1881071daf6dc8946d6d5252fded49
Rocnix <55219363+Rocnix@users.noreply.github.com>
2025-01-05 20:42:16 -0800

    create mod from template

---
+++ src/main/java/rocnix/trialsbackport/TrialsBackportClient.java
@@ -0,0 +1,10 @@
+package rocnix.trialsbackport;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrialsBackportClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/rocnix/trialsbackport/TrialsBackportDataGenerator.java
@@ -0,0 +1,11 @@
+package rocnix.trialsbackport;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TrialsBackportDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
